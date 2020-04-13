3bc2eebcfb7213e230534e1fea32d14cef0a7385
Joseph balikuddembe <baliksjosay@gmail.com>
2020-04-13 13:42:47 +0300

    Updates

---
+++ precache-manifest.9f4034e67a92b3075e05ce151c81eda5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8828e4fa119b9d870d60b8a1e249c402",
+    "url": "/free-mind-health/index.html"
+  },
+  {
+    "revision": "f3fa0f3dccc6de9ecdcc",
+    "url": "/free-mind-health/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "358383713ac5e7386584",
+    "url": "/free-mind-health/static/css/main.1e7d410e.chunk.css"
+  },
+  {
+    "revision": "f3fa0f3dccc6de9ecdcc",
+    "url": "/free-mind-health/static/js/2.d729df44.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/free-mind-health/static/js/2.d729df44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "358383713ac5e7386584",
+    "url": "/free-mind-health/static/js/main.72ab2ff1.chunk.js"
+  },
+  {
+    "revision": "71a9994defed62d5352e",
+    "url": "/free-mind-health/static/js/runtime-main.9a14f936.js"
+  },
+  {
+    "revision": "748a4ee6bda5f2007aede7ba414de2d0",
+    "url": "/free-mind-health/static/media/logo2.748a4ee6.jpg"
+  }
+]);
